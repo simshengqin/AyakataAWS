3b049ae4ced69579971a02dc39261a8fabcf29f8
simshengqin <sqsim.2018@sis.smu.edu.sg>
2021-07-18 01:49:26 +0800

    Connect view patient to DB, allow nullable fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,13 +15,9 @@ export class AppComponent implements OnInit, OnDestroy{
   title = 'authenticator';
   user: CognitoUserInterface;
   authState: AuthState;
-  public createForm!: FormGroup;
 
-  /* declare patients variable */
-  patients: Array<Patient> = [];
-  subscription: Subscription | null | undefined;
   @ViewChild('file') file: ElementRef;
-  constructor(private api: APIService, private fb: FormBuilder, private ref: ChangeDetectorRef) { }
+  constructor(private ref: ChangeDetectorRef) { }
 
   async ngOnInit() {
     onAuthUIStateChange((authState, authData) => {
@@ -29,38 +25,8 @@ export class AppComponent implements OnInit, OnDestroy{
       this.user = authData as CognitoUserInterface;
       this.ref.detectChanges();
     });
-
-    this.createForm = this.fb.group({
-      name: ['', Validators.required],
-      description: ['', Validators.required],
-      city: ['', Validators.required]
-    });
-
-    /* fetch patients when app loads */
-    this.api.ListPatients().then(event => {
-      this.patients = (event.items as Array<Patient>);
-    });
-
-    /* subscribe to new patients being created */
-    this.subscription = <Subscription> this.api.OnCreatePatientListener.subscribe((event: any) => {
-      const newPatient = event.value.data.onCreatePatient;
-      this.patients = [newPatient, ...this.patients];
-    });
   }
-  public onCreate(patient: Patient) {
-    this.api.CreatePatient(patient).then(event => {
-      this.createForm.reset();
-    })
-      .catch(e => {
-        console.log('error creating patient...', e);
-      });
-  }
-
   ngOnDestroy() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-    this.subscription = null;
     return onAuthUIStateChange;
   }
 }
--- src/app/ui/view-patient/view-patient.component.ts
+++ src/app/ui/view-patient/view-patient.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Patient} from '../../../types/patient';
+import {Subscription} from 'rxjs';
+import {APIService} from '../../API.service';
+import {onAuthUIStateChange} from '@aws-amplify/ui-components';
 
 @Component({
   selector: 'app-view-patient',
@@ -6,10 +11,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-patient.component.css']
 })
 export class ViewPatientComponent implements OnInit {
+  public createForm!: FormGroup;
 
-  constructor() { }
+  /* declare patients variable */
+  patients: Array<Patient> = [];
+  subscription: Subscription | null | undefined;
+  constructor(private api: APIService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm = this.fb.group({
+      Patient_No: ['', Validators.required],
+      Case_No: ['', Validators.required],
+      Referral_Type_From: ['', Validators.required]
+    });
+
+    /* fetch patients when app loads */
+    this.api.ListPatients().then(event => {
+      this.patients = (event.items as Array<Patient>);
+    });
+
+    /* subscribe to new patients being created */
+    this.subscription = <Subscription> this.api.OnCreatePatientListener.subscribe((event: any) => {
+      const newPatient = event.value.data.onCreatePatient;
+      this.patients = [newPatient, ...this.patients];
+    });
+  }
+  public onCreate(patient: Patient) {
+    this.api.CreatePatient(patient).then(event => {
+      this.createForm.reset();
+    })
+      .catch(e => {
+        console.log('error creating patient...', e);
+      });
   }
 
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.subscription = null;
+    return onAuthUIStateChange;
+  }
 }
